0bc79cd7a69a8379666ea0ce34f8c1115f52403a
narayanagung <agungnarayanagung@gmail.com>
2026-05-31 17:34:26 +0800

    lib/prisma typo

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {
+	prisma?: PrismaClient;
 };
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
